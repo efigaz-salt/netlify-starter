f47d3cc0152e670fb3016540c9688fd53b790fb6
Efi Gazarov <efig@salt.security>
2025-11-24 09:18:32 +0200

    change request and response

--- webpack.config.js
+++ webpack.config.js
@@ -65,71 +65,12 @@ exports.handler = async (event, context) => {
   const startTime = Date.now();
   const requestId = Math.random().toString(36).substr(2, 9);
   
-  // Capture all request information
+  // Capture all request information - raw event object
   const requestData = {
     requestId,
     functionName: '${functionName}',
     timestamp: new Date().toISOString(),
-    event: {
-      httpMethod: event.httpMethod,
-      path: event.path,
-      pathParameters: event.pathParameters,
-      queryStringParameters: event.queryStringParameters,
-      multiValueQueryStringParameters: event.multiValueQueryStringParameters,
-      headers: event.headers,
-      multiValueHeaders: event.multiValueHeaders,
-      body: event.body,
-      isBase64Encoded: event.isBase64Encoded,
-      requestContext: {
-        requestId: event.requestContext?.requestId,
-        stage: event.requestContext?.stage,
-        httpMethod: event.requestContext?.httpMethod,
-        resourceId: event.requestContext?.resourceId,
-        resourcePath: event.requestContext?.resourcePath,
-        path: event.requestContext?.path,
-        accountId: event.requestContext?.accountId,
-        apiId: event.requestContext?.apiId,
-        protocol: event.requestContext?.protocol,
-        requestTime: event.requestContext?.requestTime,
-        requestTimeEpoch: event.requestContext?.requestTimeEpoch,
-        domainName: event.requestContext?.domainName,
-        domainPrefix: event.requestContext?.domainPrefix,
-        extendedRequestId: event.requestContext?.extendedRequestId,
-        identity: {
-          accessKey: event.requestContext?.identity?.accessKey,
-          accountId: event.requestContext?.identity?.accountId,
-          apiKey: event.requestContext?.identity?.apiKey,
-          apiKeyId: event.requestContext?.identity?.apiKeyId,
-          caller: event.requestContext?.identity?.caller,
-          cognitoAuthenticationProvider: event.requestContext?.identity?.cognitoAuthenticationProvider,
-          cognitoAuthenticationType: event.requestContext?.identity?.cognitoAuthenticationType,
-          cognitoIdentityId: event.requestContext?.identity?.cognitoIdentityId,
-          cognitoIdentityPoolId: event.requestContext?.identity?.cognitoIdentityPoolId,
-          principalOrgId: event.requestContext?.identity?.principalOrgId,
-          sourceIp: event.requestContext?.identity?.sourceIp,
-          user: event.requestContext?.identity?.user,
-          userAgent: event.requestContext?.identity?.userAgent,
-          userArn: event.requestContext?.identity?.userArn,
-          acceptEncoding: event.requestContext?.identity?.acceptEncoding,
-          acceptLanguage: event.requestContext?.identity?.acceptLanguage,
-          accept: event.requestContext?.identity?.accept,
-          cacheControl: event.requestContext?.identity?.cacheControl,
-          cloudFrontForwardedProto: event.requestContext?.identity?.cloudFrontForwardedProto,
-          cloudFrontIsDesktopViewer: event.requestContext?.identity?.cloudFrontIsDesktopViewer,
-          cloudFrontIsMobileViewer: event.requestContext?.identity?.cloudFrontIsMobileViewer,
-          cloudFrontIsSmartTVViewer: event.requestContext?.identity?.cloudFrontIsSmartTVViewer,
-          cloudFrontIsTabletViewer: event.requestContext?.identity?.cloudFrontIsTabletViewer,
-          cloudFrontViewerCountry: event.requestContext?.identity?.cloudFrontViewerCountry,
-          host: event.requestContext?.identity?.host,
-          via: event.requestContext?.identity?.via,
-          xAmznTraceId: event.requestContext?.identity?.xAmznTraceId,
-          xForwardedFor: event.requestContext?.identity?.xForwardedFor,
-          xForwardedPort: event.requestContext?.identity?.xForwardedPort,
-          xForwardedProto: event.requestContext?.identity?.xForwardedProto
-        },
-        authorizer: event.requestContext?.authorizer
-      }
-    },
+    event: event,
     context: {
       functionName: context.functionName,
       functionVersion: context.functionVersion,
@@ -140,29 +81,6 @@ exports.handler = async (event, context) => {
       logStreamName: context.logStreamName,
       remainingTimeInMillis: context.getRemainingTimeInMillis ? context.getRemainingTimeInMillis() : null,
       clientContext: context.clientContext
-    },
-    httpInfo: {
-      host: event.headers?.host || event.headers?.Host,
-      origin: event.headers?.origin || event.headers?.Origin,
-      referer: event.headers?.referer || event.headers?.Referer,
-      userAgent: event.headers?.['user-agent'] || event.headers?.['User-Agent'],
-      acceptLanguage: event.headers?.['accept-language'] || event.headers?.['Accept-Language'],
-      acceptEncoding: event.headers?.['accept-encoding'] || event.headers?.['Accept-Encoding'],
-      accept: event.headers?.accept || event.headers?.Accept,
-      contentType: event.headers?.['content-type'] || event.headers?.['Content-Type'],
-      contentLength: event.headers?.['content-length'] || event.headers?.['Content-Length'],
-      authorization: event.headers?.authorization || event.headers?.Authorization,
-      cookie: event.headers?.cookie || event.headers?.Cookie,
-      xForwardedFor: event.headers?.['x-forwarded-for'] || event.headers?.['X-Forwarded-For'],
-      xForwardedProto: event.headers?.['x-forwarded-proto'] || event.headers?.['X-Forwarded-Proto'],
-      xForwardedPort: event.headers?.['x-forwarded-port'] || event.headers?.['X-Forwarded-Port'],
-      xRealIp: event.headers?.['x-real-ip'] || event.headers?.['X-Real-IP'],
-      xRequestId: event.headers?.['x-request-id'] || event.headers?.['X-Request-ID'],
-      cloudFrontViewerCountry: event.headers?.['cloudfront-viewer-country'] || event.headers?.['CloudFront-Viewer-Country'],
-      cloudFrontIsDesktop: event.headers?.['cloudfront-is-desktop-viewer'] || event.headers?.['CloudFront-Is-Desktop-Viewer'],
-      cloudFrontIsMobile: event.headers?.['cloudfront-is-mobile-viewer'] || event.headers?.['CloudFront-Is-Mobile-Viewer'],
-      cloudFrontIsTablet: event.headers?.['cloudfront-is-tablet-viewer'] || event.headers?.['CloudFront-Is-Tablet-Viewer'],
-      allHeaders: event.headers
     }
   };
 
